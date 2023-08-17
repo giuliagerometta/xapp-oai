0f634919057acd41e22db06d723b586eda7ab1d0
Giulia <giulia.gerometta@mail.polimi.it>
2023-08-17 19:25:33 +0200

    prova

--- base-xapp/monitoring_xapp.py
+++ base-xapp/monitoring_xapp.py
@@ -1,4 +1,6 @@
 import xapp_control_ricbypass
+import csv
+import time
 from e2sm_proto import *
 from time import sleep
 
@@ -12,21 +14,45 @@ def main():
 
     # internal message
     inner_mess = RAN_indication_request()
-    inner_mess.target_params.extend([RAN_parameter.GNB_ID, RAN_parameter.UE_LIST])
+    inner_mess.target_params.extend([RAN_parameter.GNB_ID, RAN_parameter.UE_LIST, RAN_parameter.CELL_LOAD])
 
     # assign and serialize
     master_mess.ran_indication_request.CopyFrom(inner_mess)
     buf = master_mess.SerializeToString()
     xapp_control_ricbypass.send_to_socket(buf)
     
-    while True:
-        r_buf = xapp_control_ricbypass.receive_from_socket()
-        ran_ind_resp = RAN_indication_response()
-        ran_ind_resp.ParseFromString(r_buf)
-        print(ran_ind_resp)
-        sleep(1)
-        xapp_control_ricbypass.send_to_socket(buf)
 
+    csv_filename = 'data.csv'
+
+    # Write header to the CSV file (if needed)
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Timestamp', 'Value'])
+
+    try:
+        while True:
+            r_buf = xapp_control_ricbypass.receive_from_socket()
+            ran_ind_resp = RAN_indication_response()
+            ran_ind_resp.ParseFromString(r_buf)
+            print(ran_ind_resp)
+            sleep(0.5) # Wait for 500 milliseconds
+            xapp_control_ricbypass.send_to_socket(buf)
+            
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+            value = r_buf  # Replace this with your actual data
+
+            data = [timestamp, value]
+            append_to_csv(csv_filename, data)
+
+            
+    except KeyboardInterrupt:
+        print("Data collection stopped.")
+
+
+def append_to_csv(filename, data):
+    with open(filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(data)
 
 if __name__ == '__main__':
     main()
