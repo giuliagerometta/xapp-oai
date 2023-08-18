a3b6a21ce80aa676b2a78d02c86e5698133667c0
Giulia <giulia.gerometta@mail.polimi.it>
2023-08-18 14:36:36 +0200

    prova

--- base-xapp/monitoring_xapp.py
+++ base-xapp/monitoring_xapp.py
@@ -34,12 +34,12 @@ def main():
             r_buf = xapp_control_ricbypass.receive_from_socket()
             ran_ind_resp = RAN_indication_response()
             ran_ind_resp.ParseFromString(r_buf)
-            value = ran_ind_resp.json()
+            value = ran_ind_resp.ue_list
             print(ran_ind_resp)
             sleep(0.5) # Wait for 500 milliseconds
             xapp_control_ricbypass.send_to_socket(buf)
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-            data = [timestamp, value['ue_rsrp']]
+            data = [timestamp, value]
             append_to_csv(csv_filename, data)
 
             
