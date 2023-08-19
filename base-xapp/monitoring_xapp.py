6ee6e1ec9f8359a8c502ec39d4b7b697a802d34c
Giulia <giulia.gerometta@mail.polimi.it>
2023-08-19 14:23:21 +0200

    prova

--- base-xapp/monitoring_xapp.py
+++ base-xapp/monitoring_xapp.py
@@ -7,15 +7,8 @@ import os
 
 def main():    
 
-    csv_filename = 'data.csv'
-    try:
-        with open(csv_filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(['Timestamp', 'Value'])
-    except Exception as e:
-        print("Error creating CSV file:", e)
-
     print("Encoding ric monitoring request")
+    print("Current working directory:", os.getcwd())
     
     # external message
     master_mess = RAN_message()
@@ -30,9 +23,15 @@ def main():
     buf = master_mess.SerializeToString()
     xapp_control_ricbypass.send_to_socket(buf)
     
-    with open(csv_filename, 'a', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-    
+
+    csv_filename = 'data.csv'
+    try:
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Timestamp', 'Value'])
+    except Exception as e:
+        print("Error creating CSV file:", e)
+
     try:
         while True:
             r_buf = xapp_control_ricbypass.receive_from_socket()
@@ -51,8 +50,7 @@ def main():
                         
                         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
                         data = [timestamp, ue_rsrp]
-                        csvwriter.writerow(data)
-
+                        append_to_csv(csv_filename, data)
             
             print(ran_ind_resp)
             sleep(0.5) # Wait for 500 milliseconds
@@ -60,10 +58,14 @@ def main():
             
     except KeyboardInterrupt:
         print("Data collection stopped.")
-        print("Closing CSV file.")
+        print("Closing the CSV file.")
         csvfile.close()
 
-        
+
+def append_to_csv(filename, data):
+    with open(filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(data)
 
 if __name__ == '__main__':
     main()
