7d55a717f417b6269fcc1655eba7a61ba03761e2
Giulia <giulia.gerometta@mail.polimi.it>
2023-08-19 14:10:25 +0200

    prova

--- base-xapp/monitoring_xapp.py
+++ base-xapp/monitoring_xapp.py
@@ -7,8 +7,15 @@ import os
 
 def main():    
 
+    csv_filename = 'data.csv'
+    try:
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Timestamp', 'Value'])
+    except Exception as e:
+        print("Error creating CSV file:", e)
+
     print("Encoding ric monitoring request")
-    print("Current working directory:", os.getcwd())
     
     # external message
     master_mess = RAN_message()
@@ -23,15 +30,9 @@ def main():
     buf = master_mess.SerializeToString()
     xapp_control_ricbypass.send_to_socket(buf)
     
-
-    csv_filename = 'data.csv'
-    try:
-        with open(csv_filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(['Timestamp', 'Value'])
-    except Exception as e:
-        print("Error creating CSV file:", e)
-
+    with open(csv_filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+    
     try:
         while True:
             r_buf = xapp_control_ricbypass.receive_from_socket()
@@ -50,7 +51,8 @@ def main():
                         
                         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
                         data = [timestamp, ue_rsrp]
-                        append_to_csv(csv_filename, data)
+                        csvwriter.writerow(data)
+
             
             print(ran_ind_resp)
             sleep(0.5) # Wait for 500 milliseconds
@@ -58,12 +60,10 @@ def main():
             
     except KeyboardInterrupt:
         print("Data collection stopped.")
+        print("Closing CSV file.")
+        csvfile.close()
 
-
-def append_to_csv(filename, data):
-    with open(filename, 'a', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(data)
+        
 
 if __name__ == '__main__':
     main()
